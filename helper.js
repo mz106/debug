17984403a37b8c91d869bdc1af2029f655b2243c
mz106 <flashman123@fastmail.fm>
2021-09-27 13:29:50 +0100

    three bugs added

--- helper.js
+++ helper.js
@@ -1,25 +1,16 @@
 
-const { employees, bonus } = require('./data.js');
 const checkedEmployees = [];
 
-const checkName = (arr1, arr2) => {
-    for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr2.length; j++) {
-            if(arr1[i] === arr2[j]) {
-                checkedEmployees.push(arr2[j]);   
+const checkName = (employees, bonus) => {
+    for (let i = 0; i < employees.length; i++) {
+        for (let i = 0; i < bonus.length; i++) {
+            if(employees[i] === bonus[i]) {
+                checkedEmployees.push(bonus[i]);   
             } 
         }
     }
-    console.log(checkedEmployees);
+    
 };
-
-// if(name === arr[i]) {
-//     checkedEmployees.push(arr[i]);
-//     console.log(checkedEmployees);
-// } else {
-//     console.log('not on list')
-// }
-
 
 module.exports = {
     checkName
--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const { employees, bonus } = require('./data.js');
 const { checkName } = require('./helper');
 const payDay = true;
 
+
 //checks if name is on the list of people at the company and greets them.
 
-const sayHello = (par, arr1, arr2) => {
+const sayHello = (payDay, employees, bonus) => {
     if(payDay) {
-        checkName(arr1, arr2);
+        const names = checkName(employees, bonus);
+        for(let i = 0; i < names.length; i++) {
+            console.log(`${names[i]} has been given a bonus`);
+        }
     } else {
         console.log(`It's not payday`);
     }
 }
-
-sayHello(payDay, bonus, employees);
+sayHello(payDay, employees, bonus);
